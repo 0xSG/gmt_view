8610e395dec4899c42b6fd2204e919da46f7aba1
0xPulsar <sooryagangaraj@sooryas-MacBook-Pro.local>
2018-11-11 12:25:04 +0530

    Update src/index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
+//vulnerability fixed.
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
